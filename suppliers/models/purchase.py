d92038512de2e3d487f7a57534c1146a7ac8d488
tatendatembojnr-code <tatendatembojnr@gmail.com>
2026-06-19 08:24:20 +0200

    Convert item_code to Many2one and isolate display names using contexts

--- suppliers/models/purchase.py
+++ suppliers/models/purchase.py
@@ -69,7 +69,7 @@ class PurchaseLine(models.Model):
 
     purchase_id = fields.Many2one('havanoposdesk.purchase', string='Purchase', required=True, ondelete='cascade')
     product_id = fields.Many2one('havanoposdesk.product', string='Item Name', required=True)
-    item_code = fields.Char(string='Item Code')
+    item_code = fields.Many2one('havanoposdesk.product', string='Item Code')
     accepted_qty = fields.Float(string='Accepted Quantity', default=1.0)
     rate = fields.Float(string='Rate')
     amount = fields.Float(string='Amount', compute='_compute_amount', store=True)
@@ -82,13 +82,11 @@ class PurchaseLine(models.Model):
     @api.onchange('item_code')
     def _onchange_item_code(self):
         if self.item_code:
-            product = self.env['havanoposdesk.product'].search([('item_code', '=', self.item_code)], limit=1)
-            if product:
-                self.product_id = product.id
-                self.rate = product.buying_price
+            self.product_id = self.item_code
+            self.rate = self.item_code.buying_price
 
     @api.onchange('product_id')
     def _onchange_product_id(self):
         if self.product_id:
-            self.item_code = self.product_id.item_code
+            self.item_code = self.product_id
             self.rate = self.product_id.buying_price
